ad8e695fa68dfa4fb18cba769db60c5198eb90e5
CHEN WANG <wangchen@cn.ibm.com>
2020-07-13 16:40:23 +0800

    initial commit

---
+++ src/main/java/application/ExampleEndpoint.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleEndpoint {
+
+    @RequestMapping("/starter/hello")
+    public String example() {
+        return "Hello World Demo Inside";
+    }
+}
